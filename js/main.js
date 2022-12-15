172440d0ebaf1c5a58fa3c02db25dea08876ce57
NazarFedus <simpleprograming7@gmail.com>
2022-12-15 13:09:31 +0000

    header with adaptation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+// nav mobile menu
+const navBtn = document.querySelector('.nav__toggle');
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav');
+const navlist = document.querySelector('.nav__list');
+const navbtnmobile = document.querySelector('.nav__btn');
+const menuiconwrapper = document.querySelector('.menu-icon-wrapper');
+
+navBtn.onclick = function(){
+     nav.classList.toggle('nav--mobile'); // додаєм клас nav--active до nav
+     menuIcon.classList.toggle('menu-icon--active'); // додаєм клас menu-icon--active до menu-icon
+     document.body.classList.toggle('no-scroll'); // додаєм клас no-scroll до body
+     navlist.classList.toggle('nav__list--active'); // додаєм клас nav__list--active до nav__list
+     navbtnmobile.classList.toggle('nav__btn--active'); // додаєм клас nav__btn--active до nav__btn
+     menuiconwrapper.classList.toggle('menu-icon-wrapper--active'); // додаєм клас menu-icon-wrapper--active до menu-icon-wrapper
+}
+
+
+
+// Header img
 const img = document.querySelector('.header__main-img');
 
 repeat();
@@ -20,4 +40,5 @@ function repeat(){
           }
      }
 }
+
 
